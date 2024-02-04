45daba70b01a74347d444128652c8b0279a9f229
phoenix <ayomikuntemitope246@gmail.com>
2024-02-04 18:04:51 +0000

    added ci

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
-
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +8,32 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#1B0354",
+        secondary: "#3a3f51",
+        "main-sec": "#0D062D",
+        success: "#34CAA5",
+        error: "#ff1f7d",
+        warning: "#f5bf3d",
+        light: "#78828A",
+        "soft-light": "#DADDDD",
+        header: "#26282c",
+        "soft-border": "#ebecf2",
+        "soft-bg": "#f7f8fa",
+        "trend-down": "#ed544e",
+        purple: {
+          50: "#ECEBFF",
+          600: "#574EFA",
+          900: "#33059F",
+          950: "#1B0354",
+        },
+        sh: "rgba(16,24,40,0.05)",
+        neutraly: "#6B7B8F",
+        destructive: "#f81d28",
       },
+    },
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
     },
   },
   plugins: [],
