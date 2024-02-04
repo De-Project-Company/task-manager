59fb95472d244e3d16c34b97bdce6f2dd0226a00
phoenix <ayomikuntemitope246@gmail.com>
2024-02-04 23:18:09 +0000

    home layout

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,69 @@
+import React from "react";
+import {
+  type Icon,
+  Category,
+  Briefcase,
+  WalletMoney,
+  MessageText,
+  Personalcard,
+  Notification1,
+  Profile2User,
+  ArchiveBook,
+  TrendUp,
+  Box,
+  DiscountShape,
+  InfoCircle,
+  ArrowCircleRight2,
+  Setting2,
+  Logout,
+  BoxTick,
+  I3DRotate,
+  ShoppingCart,
+  Coin1,
+  Message,
+} from "iconsax-react";
+import { SidebarProps } from "@/types";
+
+export const EMPLOYERSSIDEBAR_LINKS: SidebarProps[] = [
+  {
+    id: 1,
+    label: "Client Dashboard",
+    icon: Category,
+    link: "dashboard",
+  },
+
+  {
+    id: 2,
+    label: "Total Projects",
+    icon: TrendUp,
+    link: "total-projects",
+  },
+  {
+    id: 3,
+    label: "Meetings",
+    icon: Profile2User,
+    link: "Meetings",
+  },
+  {
+    id: 4,
+    label: "Chats",
+    icon: Message,
+    link: "chat",
+  },
+  {
+    id: 5,
+    label: "Notification",
+    icon: Notification1,
+    link: "notification",
+  },
+  {
+    id: 6,
+    label: "settings",
+    icon: Setting2,
+    link: "settings",
+  },
+];
+
 const NAVLINKS = [
   {
     title: "Home",
@@ -45,3 +111,5 @@ export const Aboutdata = [
   "Foster transparent communication with clients and team members, creating a collaborative environment that drives success.",
   "With offline support and auto-sync, Traverse adapts to your work style, allowing seamless time tracking regardless of your location.",
 ];
+
+export const TYPESidebarLinks = EMPLOYERSSIDEBAR_LINKS.map((link) => link.link);
