d44cf233519447c2b730fa594d712d2c22cc9800
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-02-14 00:48:26 +0000

    project

--- utils/index.ts
+++ utils/index.ts
@@ -27,8 +27,6 @@ export const shrinkString = ({
   return str;
 };
 
-
-
 /**
  * Returns an Encrypted a string .
  * @function encryptString - Encodes or encrypts a string using a base64 Buffer
@@ -36,7 +34,7 @@ export const shrinkString = ({
  */
 export const encryptString = (str: string): string => {
   const buffer = Buffer.from(str);
-  return buffer.toString('base64');
+  return buffer.toString("base64");
 };
 
 /**
@@ -46,8 +44,83 @@ export const encryptString = (str: string): string => {
  */
 
 export const decryptString = (str: string): string => {
-  const buffer = Buffer.from(str, 'base64');
+  const buffer = Buffer.from(str, "base64");
   return buffer.toString();
+};
+
+/**
+ * Returns a string representing the time elapsed since the given time.
+ * @param time - The time in seconds or a Date object.
+ * @returns The formatted time string.
+ */
+export const commentsTime = (time: number | Date): string => {
+  function getSuffix(date: number): string {
+    if (date === 1 || date === 21 || date === 31) {
+      return "st";
+    } else if (date === 2 || date === 22) {
+      return "nd";
+    } else if (date === 3 || date === 23) {
+      return "rd";
+    } else {
+      return "th";
+    }
+  }
+
+  time = new Date((time as number) * 1000);
+  let str = "";
+
+  let months: string[] = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  let chunks: [number, string][] = [
+    [31536000000, "year"],
+    [2592000000, "month"],
+    [604800000, "week"],
+    [86400000, "day"],
+    [3600000, "hour"],
+    [60000, "minute"],
+    [1000, "second"],
+  ];
+
+  let today: Date = new Date();
+  let since: Date = new Date(today.getTime() - time.getTime());
+
+  if (since.getTime() > 604800000) {
+    str = `${months[time.getMonth()]} ${time.getDate()} ${getSuffix(
+      time.getDate()
+    )}`;
+
+    if (since.getTime() > 31536000000) {
+      str = `${str}, ${time.getFullYear()}`;
+    }
+    return str;
+  }
+
+  let ms: number = 0;
+  let name: string = "";
+  let i: number = 0;
+  let chunksLen: number = chunks.length;
+  let count: number = 0;
+  for (i = 0; i < chunksLen; i++) {
+    ms = chunks[i][0];
+    name = chunks[i][1];
+    count = Math.floor(since.getTime() / ms);
+    if (count !== 0) break;
+  }
+
+  return `${count} ${name}${count > 1 ? "s" : ""} ago`;
 };
 
 /**
@@ -59,7 +132,6 @@ export const generateId = () => {
   return Math.random().toString(36).substring(2, 9);
 };
 
-
 /**
  * Returns the name from an email address.
  * @function getNameFromEmail
@@ -68,7 +140,7 @@ export const generateId = () => {
  */
 
 export const getNameFromEmail = (email: string): string => {
-  return email.split('@')[0];
+  return email.split("@")[0];
 };
 
 /**
@@ -78,15 +150,13 @@ export const getNameFromEmail = (email: string): string => {
  * @returns {string}
  */
 export const getInitials = (name: string): string => {
-  const words = name.split(' ');
+  const words = name.split(" ");
   return words
-    .map(word => word[0])
+    .map((word) => word[0])
     .slice(0, 2)
-    .join('')
+    .join("")
     .toUpperCase();
 };
-
-
 
 /**
  * Sets a key-value pair in the session storage.
@@ -96,7 +166,7 @@ export const getInitials = (name: string): string => {
  */
 
 export const SetToSessionStorage = (key: string, value: any): void => {
-  if (typeof sessionStorage === 'undefined') return;
+  if (typeof sessionStorage === "undefined") return;
   try {
     const serializedValue = JSON.stringify(value);
     sessionStorage.setItem(key, serializedValue);
@@ -113,7 +183,7 @@ export const SetToSessionStorage = (key: string, value: any): void => {
  */
 
 export const GetFromSessionStorage = (key: string): string | null => {
-  if (typeof sessionStorage === 'undefined') return null;
+  if (typeof sessionStorage === "undefined") return null;
   try {
     const storedValue = sessionStorage?.getItem(key);
     return storedValue !== null ? storedValue : null;
