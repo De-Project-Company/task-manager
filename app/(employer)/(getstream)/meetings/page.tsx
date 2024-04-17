ade3ef3caf86343dcdf357bd0bb3689fd0c22e23
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-04-17 12:52:07 +0100

    private meeting draft

--- app/(employer)/(getstream)/meetings/page.tsx
+++ app/(employer)/(getstream)/meetings/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MeetingNav from "./content/Nav";
+import MyMeetingsPage from "./content/MyMeetings";
 
 const MeetingPage = () => {
   return (
     <div>
       <MeetingNav />
+      <MyMeetingsPage />
     </div>
   );
 };
