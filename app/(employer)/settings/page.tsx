f2a5945b38976932d2189c5f17dd6ad24488ff72
hisreal123 <omosgoerge@gmail.com>
2024-04-23 14:01:19 -0500

    Update: coming soon pages

---
+++ app/(employer)/settings/page.tsx
@@ -0,0 +1,12 @@
+import CommingSoon from "@/components/ComminSoon";
+import React from "react";
+
+const page: React.FC = () => {
+  return (
+    <div className="px-3 bg-white">
+      <CommingSoon />
+    </div>
+  );
+};
+
+export default page;
---
+++ components/ComminSoon.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+
+const CommingSoon: React.FC = () => {
+  return (
+    <>
+      <div className="w-full flex justify-center items-center relative h-[500px] bg-white">
+        <div className="wrap flex flex-col items-center space-y-5">
+          <h1 className=" text-2xl md:text-3xl font-bold ">Comming Soon</h1>
+          <Link
+            href="/dashboard"
+            className="block py-1 px-2  bg-primary/90 hover:bg-primary text-white w-fit rounded-lg"
+          >
+            Back to Home
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CommingSoon;
