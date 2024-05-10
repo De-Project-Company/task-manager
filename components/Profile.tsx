8435c47d284e91fce46f17bd8991d33146ef251e
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2024-05-10 19:58:30 +0100

    Update Profile.tsx

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -111,7 +111,7 @@ const Profile = () => {
         <SheetHeader>
           <SheetTitle>Edit profile</SheetTitle>
           <SheetDescription>
-            Make changes to your profile here. Click save when you're done.
+            Make changes to your profile here. Click save when you&apos;re done.
           </SheetDescription>
         </SheetHeader>
         <form onSubmit={handleSubmit} className="grid gap-4 py-4">
